51c08dbbf482fd900c2baf40501fe9cbdde08e93
Aryan Das <Aryan@Prithvirajs-iMac.local>
2021-02-03 16:49:17 -0800

    Blog added

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,12 @@ class Blog(models.Model):
     title = models.CharField(max_length=20)
     pub_date = models.DateTimeField()
 
+    def summary(self):
+        return self.body[:50]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
+
